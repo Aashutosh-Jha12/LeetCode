fe8ed73c1a7f0af81fc7cfeb8fbf8e92832bf47e
Aashutosh Jha <aashutoshj55@gmail.com>
2026-07-05 21:24:25 +0545

    Time: 0 ms (100%), Space: 8.5 MB (86.9%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0;
+        // if (x == 0) return 0;
 
-        int ans = 0;
-        for (long long i = 1; i * i <= x; i++) {
-            ans = i;
+        // int ans = 0;
+        // for (long long i = 1; i * i <= x; i++) {
+        //     ans = i;
+        // }
+        // return ans;
+        if (x==0)   return 0;
+        int left=1,right=x;
+        int ans=0;
+
+        while( left<=right){
+            long long mid= left + ( right - left) / 2;
+            if ( mid * mid ==x){
+                return mid;
+            }
+            else if ( mid * mid <x){
+                ans= mid;
+                left= mid+1;
+            }
+            else{
+                right = mid -1;
+            }
+            
         }
         return ans;
+
     }
+    
 };
