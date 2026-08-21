6bc6328dad75c7500cb435ad6abc69321c47e1f5
Aashutosh Jha <aashutoshj55@gmail.com>
2026-08-21 23:59:07 +0545

    Time: 0 ms (100%), Space: 8.5 MB (93.31%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;  // Important: handles INT_MIN safely
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double res = 1.0;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                res *= x;
+            }
+
+            x *= x;
+            N /= 2;
+        }
+
+        return res;
+    }
+};
